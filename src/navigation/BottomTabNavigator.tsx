c9e4401ee6aa87bfa942751e18fb7314bd9635d3
Carsten Koch <carsten.b.koch@gmail.com>
2021-01-16 18:42:36 +0100

    feat: navigation updated

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,73 +1,95 @@
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator } from '@react-navigation/stack'
+import * as React from 'react'
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
+import Colors from '../constants/Colors'
+import useColorScheme from '../hooks/useColorScheme'
+import TrainingsScreen from '../screens/TrainingsScreen'
+import HistoryScreen from '../screens/HistoryScreen'
+import ProfileScreen from '../screens/ProfileScreen'
+import { BottomTabParamList, TrainingsParamList, HistoryParamList, ProfileParamList } from '../types'
 
-const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+const BottomTab = createBottomTabNavigator<BottomTabParamList>()
 
 export default function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
+    const colorScheme = useColorScheme()
 
-  return (
-    <BottomTab.Navigator
-      initialRouteName="TabOne"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
-      <BottomTab.Screen
-        name="TabOne"
-        component={TabOneNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-        }}
-      />
-      <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-        }}
-      />
-    </BottomTab.Navigator>
-  );
+    return (
+        <BottomTab.Navigator
+            initialRouteName="Trainings"
+            tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
+            <BottomTab.Screen
+                name="Trainings"
+                component={TrainingsNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <TabBarIcon name="running" color={color} />,
+                }}
+            />
+            <BottomTab.Screen
+                name="History"
+                component={HistoryNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <TabBarIcon name="history" color={color} />,
+                }}
+            />
+            <BottomTab.Screen
+                name="Profile"
+                component={ProfileNavigator}
+                options={{
+                    tabBarIcon: ({ color }) => <TabBarIcon name="user-alt" color={color} />
+                }}
+            />
+        </BottomTab.Navigator>
+    )
 }
 
 // You can explore the built-in icon families and icons on the web at:
 // https://icons.expo.fyi/
 function TabBarIcon(props: { name: string; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+    return <FontAwesome5 size={30} style={{ marginBottom: -3 }} {...props} />
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
 // https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
-const TabOneStack = createStackNavigator<TabOneParamList>();
+const TrainingsStack = createStackNavigator<TrainingsParamList>()
 
-function TabOneNavigator() {
-  return (
-    <TabOneStack.Navigator>
-      <TabOneStack.Screen
-        name="TabOneScreen"
-        component={TabOneScreen}
-        options={{ headerTitle: 'Tab One Title' }}
-      />
-    </TabOneStack.Navigator>
-  );
+function TrainingsNavigator() {
+    return (
+        <TrainingsStack.Navigator>
+            <TrainingsStack.Screen
+                name="TrainingsScreen"
+                component={TrainingsScreen}
+                options={{ headerTitle: 'Trainings' }}
+            />
+        </TrainingsStack.Navigator>
+    )
 }
 
-const TabTwoStack = createStackNavigator<TabTwoParamList>();
+const HistoryStack = createStackNavigator<HistoryParamList>()
 
-function TabTwoNavigator() {
-  return (
-    <TabTwoStack.Navigator>
-      <TabTwoStack.Screen
-        name="TabTwoScreen"
-        component={TabTwoScreen}
-        options={{ headerTitle: 'Tab Two Title' }}
-      />
-    </TabTwoStack.Navigator>
-  );
+function HistoryNavigator() {
+    return (
+        <HistoryStack.Navigator>
+            <HistoryStack.Screen
+                name="HistoryScreen"
+                component={HistoryScreen}
+                options={{ headerTitle: 'History' }}
+            />
+        </HistoryStack.Navigator>
+    )
+}
+
+const ProfileStack = createStackNavigator<ProfileParamList>()
+
+function ProfileNavigator() {
+    return (
+        <ProfileStack.Navigator>
+            <ProfileStack.Screen
+                name="ProfileScreen"
+                component={ProfileScreen}
+                options={{ headerTitle: "Profile" }}
+            />
+        </ProfileStack.Navigator>
+    )
 }
---
+++ src/types.tsx
@@ -0,0 +1,22 @@
+export type RootStackParamList = {
+    Root: undefined
+    NotFound: undefined
+}
+
+export type BottomTabParamList = {
+    Trainings: undefined
+    History: undefined
+    Profile: undefined
+}
+
+export type TrainingsParamList = {
+    TrainingsScreen: undefined
+}
+
+export type HistoryParamList = {
+    HistoryScreen: undefined
+}
+
+export type ProfileParamList = {
+    ProfileScreen: undefined
+}
